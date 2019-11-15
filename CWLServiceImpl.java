cd8e3fdb8bfa57a5f48d195ca498085b6f9550e8
nishitbhansali <43116066+nishitbhansali@users.noreply.github.com>
2019-11-14 20:04:59 -0500

    added variable5

--- CWLServiceImpl.java
+++ CWLServiceImpl.java
@@ -1,6 +1,6 @@
 private int variable1;
 private int variable2;
-
+private int variable6;// added for conflict in WBL_R2019_11 for a conflict with master variable5
 private int variable3;
 private int variable4; // Added for WBL_R2019_11 branch
 echo "Thank you";
